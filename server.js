c7a8e6a1083f1ba4722a9b2a4125e658bc18570a
Wayne <wayne.c.griffiths@gmail.com>
2020-04-30 23:36:12 +0800

    install dependency open, update server.js

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
+const open = require('open');
 const express = require('express');
 const path = require('path');
+
+const app = express();
+const PORT = 8080;
+const localhost = `http://localhost${PORT}`
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routing
+
+
+//Starts server 
+app.listen(PORT, () => {
+    console.log(`Server running at ${localhost}`);
+    open(localhost);
+});
+
