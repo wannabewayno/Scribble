6ebfd14969042265e3bf7d5ae42ef72afe7cd734
Wayne <wayne.c.griffiths@gmail.com>
2020-05-01 00:22:40 +0800

    Add express middleware to serve static files

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ const app = express();
 const PORT = 8080;
 const localhost = `http://localhost:${PORT}`
 
+// Sets up Express app to load static files from 'public'
+app.use(express.static(path.join('public')));
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Routing
-
+//========= Routes ========
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+});
+app.get('/notes',(req,res) => {
+    res.sendFile(path.join(__dirname,"./public/notes.html"))
+})
 
 //Starts server 
 app.listen(PORT, () => {
